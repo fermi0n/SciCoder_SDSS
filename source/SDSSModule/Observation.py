cd49a0735375ff7348e8dcf793726da2959058ec
Dom Mendonca <dom.mendonca@energytailors.com.au>
2018-11-20 17:08:20 +1100

    Merged Spectrum.py and added more variables. About to start working on loading the fits file.

--- source/SDSSModule/Observation.py
+++ source/SDSSModule/Observation.py
@@ -1,11 +1,34 @@
+import Spectrum
+from astropy.io import fits
+import numpy as np
+
+class Spectrum (Observation):
+
+    def __init__(self):
+        self.flux = None
+        self.wavelength = None
+        self.plate = None
+        self.mjd = None
+        self.fibre = None
 
 class Observation:
 
-    def __init__(self):
+    def __init__(self, filename):
 
-        self.ra = None
-        self.dec = None
-        self.redshift = None
-        self.timestamp = None
+        self.ra = None      #will be a float
+        self.dec = None     #will be a float
+        self.redshift = None   #will be a float
+        self.timestamp = None   #will be a timestamp
+        #self.spectrum = None    #will be a Spectrum object
+        self.plate = None
+        self.mjd = None
+        self.fibre = None
+        self.flux = None    #numpy arrays of length X
+        self.wavelength = None   #numpy arrays of length X (same as above)
 
-    
+        #Load fits file, called filename
+        #load into the above variables
+
+
+
+
