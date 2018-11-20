f19fef65cd39ff6bbad626b2e2aa0e281b60c51a
Craig Ingram <craig.ingram@adelaide.edu.au>
2018-11-20 20:40:23 +1100

    Added method to plot spectra Added self.name to use in plt.title, maybe more versatile than `print`???

--- source/SDSSModule/Observation.py
+++ source/SDSSModule/Observation.py
@@ -1,6 +1,6 @@
 from astropy.io import fits
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 # class Spectrum (Observation):
 
@@ -24,6 +24,8 @@ class Observation(object):
         self.fiber = None
         self.flux = None  # numpy arrays of length X
         self.wavelength = None  # numpy arrays of length X (same as above)
+        self.name = None
+
 
         # Load fits file, called filename
         # load into the above variables
@@ -56,5 +58,20 @@ class Observation(object):
         fiber_string = str(self.fiber)
         while len(str(fiber_string)) != 4:
             fiber_string = '0' + fiber_string
-
+        self.name = (f"{self.plate}-{self.mjd}-" + fiber_string)
         print(f"{self.plate}-{self.mjd}-" + fiber_string)
+
+    # This method allows plotting of spectra
+    def spectra_plot(self):
+        print(type(self.wavelength))
+        plt.figure()
+        plt.plot(self.wavelength,self.flux)
+        plt.grid()
+        x1 = self.wavelength[0]
+        x2 = self.wavelength[len(self.wavelength)-1]
+        plt.xlim([x1,x2])
+        plt.xlabel(r'Wavelength ($\AA$)')
+        plt.ylabel('Flux ')
+        plt.title(self.name)
+
+        plt.show()
