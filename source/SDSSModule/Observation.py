0daee3ff3818dc8073bc5158cdb642df7eda3d49
JackHon55 <jack_hon55@hotmail.com>
2018-11-21 11:26:11 +1100

    Merged Craig's work with Jack's. Dealt with inconsistencies and reworked spectrum name to be a lazy loading property

--- source/SDSSModule/Observation.py
+++ source/SDSSModule/Observation.py
@@ -65,14 +65,13 @@ class Observation(object):
     def spectra_plot(self):
 
         plt.figure()
-        plt.plot(self.wavelength,self.flux, linewidth = 0.4)
-        plt.grid(linewidth = 0.4, linestyle='--', alpha = 0.6)
+        plt.plot(self.wavelength, self.flux, linewidth=0.4)
+        plt.grid(linewidth=0.4, linestyle='--', alpha=0.6)
         x1 = self.wavelength[0]
-        x2 = self.wavelength[len(self.wavelength)-1]
-        plt.xlim([x1,x2])
-        plt.xlabel(r'Wavelength []$\AA$]')
+        x2 = self.wavelength[len(self.wavelength) - 1]
+        plt.xlim([x1, x2])
+        plt.xlabel(r'Wavelength [$\AA$]')
         plt.ylabel('Flux [F$_{\lambda}$]')
-        plt.title(self.name)
-
+        plt.title(self.spectrum_name)
 
         plt.show()
